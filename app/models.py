65d227e1dd2fd332e47fd3f564e89f34b0388a4d
leAyush98 <rod999lee@gmail.com>
2023-05-26 14:04:16 +0530

    third

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    GENDER = [("Male", "Male"), ("Female" ,"Female")]
+    name = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=30, choices=GENDER)
+
+    def __str__(self) -> str:
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PersonForm
+from .models import Person
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, "index.html", {})
